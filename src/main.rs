c3d5ad8aced0d5d5ce65a171b3b5a36eb9e1a6b6
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-09-02 08:54:22 -0700

    improvement: add day 2 setlist

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use rocket::fs::{relative, FileServer};
 use rocket::http::Status;
 use rocket_dyn_templates::{context, Template};
 
+const DAY_1_SET_LIST: [&str; 17] = ["Creeping Death", "Harvester of Sorrow", "Leper Messiah", "King Nothing", "72 Seasons", "If Darkness Had a Son", "Orca Attack (Rob and Kirk jam)", "The Day That Never Comes", "Shadows Follow", "Orion", "Kirk Solo", "Nothing Else Matters", "Sad But True", "Blackened", "Fuel", "Seek and Destroy", "Master of Puppets"];
+
+const DAY_2_SET_LIST: [&str; 16] = ["Whiplash", "For Whom the Bell Tolls", "Ride the Lightning", "The Memory Remains", "Lux Aeterna", "Screaming Suicide", "Rob and Kirk jam (Sonics)", "Welcome Home (Sanitarium)", "Wherever I May Roam", "The Call of Cthulu", "The Unforgiven", "Inamorata", "Fight Fire with Fire", "Moth into Flame", "One", "Enter Sandman"];
+
 // function which returns a 200 OK response with empty body
 #[get("/health")]
 fn health() -> Status {
@@ -15,8 +19,8 @@ fn health() -> Status {
 // function which returns the index page template
 #[get("/")]
 fn index() -> Template {
-    let set_list = ["Creeping Death", "Harvester of Sorrow", "Leper Messiah", "King Nothing", "72 Seasons", "If Darkness Had a Son", "Orca Attack (Rob and Kirk jam)", "The Day That Never Comes", "Shadows Follow", "Orion", "Kirk Solo", "Nothing Else Matters", "Sad But True", "Blackened", "Fuel", "Seek and Destroy", "Master of Puppets"];
-    Template::render("index", context! { set_list })
+    let set_lists = (DAY_1_SET_LIST, DAY_2_SET_LIST);
+    Template::render("index", context! { set_lists })
 }
 
 // function to create a rocket instance
