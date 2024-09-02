27e59a508803ac3c5a00791424cd97754296e6b5
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-09-02 10:38:21 -0700

    improvement: tweak layout and styles

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use rocket_dyn_templates::{context, Template};
 
 const DAY_1_SET_LIST: [&str; 17] = ["Creeping Death", "Harvester of Sorrow", "Leper Messiah", "King Nothing", "72 Seasons", "If Darkness Had a Son", "Orca Attack (Rob and Kirk jam)", "The Day That Never Comes", "Shadows Follow", "Orion", "Kirk Solo", "Nothing Else Matters", "Sad But True", "Blackened", "Fuel", "Seek and Destroy", "Master of Puppets"];
 
-const DAY_2_SET_LIST: [&str; 16] = ["Whiplash", "For Whom the Bell Tolls", "Ride the Lightning", "The Memory Remains", "Lux Aeterna", "Screaming Suicide", "Rob and Kirk jam (Sonics)", "Welcome Home (Sanitarium)", "Wherever I May Roam", "The Call of Cthulu", "The Unforgiven", "Inamorata", "Fight Fire with Fire", "Moth into Flame", "One", "Enter Sandman"];
+const DAY_2_SET_LIST: [&str; 16] = ["Whiplash", "For Whom the Bell Tolls", "Ride the Lightning", "The Memory Remains", "Lux AEterna", "Screaming Suicide", "Rob and Kirk jam (Sonics)", "Welcome Home (Sanitarium)", "Wherever I May Roam", "The Call of Cthulu", "The Unforgiven", "Inamorata", "Fight Fire with Fire", "Moth into Flame", "One", "Enter Sandman"];
 
 // function which returns a 200 OK response with empty body
 #[get("/health")]
